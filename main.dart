abcab481bb51e64c0742776216532bd545596dd4
Abir Hossain <160416353+Abir117@users.noreply.github.com>
2026-05-07 13:20:25 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,8 +5,7 @@ import 'package:tech_borrow/app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  // NOTE: If you haven't run 'flutterfire configure' yet, 
-  // you might need to provide options here or add the generated firebase_options.dart file.
+
   await Firebase.initializeApp();
   
   runApp(const TechborrowApp());
